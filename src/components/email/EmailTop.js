4d8852ccb10a0bf07ae14279e859bcf612bab1fd
Your Name <you@example.com>
2024-10-23 22:00:21 +0530

    add(23/10/24)

--- src/components/email/EmailTop.js
+++ src/components/email/EmailTop.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import "../../asset/styles/email/emailtop.css";
+import emailtop from "../../asset/images/feature3.svg";
 
 const EmailTop = () => {
   return (
-    <div>
-      welcome
+    <div className="email_top">
+    <div className="email_top_image">
+      <img src={emailtop} alt="top"/>
     </div>
+    <div className="email_top_detail">
+      <h1>Email marketing that<br></br>
+      works seamlessly<br></br>
+      with the rest of<br></br>
+      your business</h1>
+      <h4>From weekly newsletters to segmented sales<br></br>
+       pitches, Podia Email helps you build your audience,<br></br>
+        nurture your relationships, and make more sales.</h4>
+      <button className="email_top_button">Start your free trial</button>
+    </div>
+  </div>
   );
 }
 
