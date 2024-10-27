4a40d6cfef8d01bb28f4c1020f16aca613e1e137
Your Name <you@example.com>
2024-10-27 21:10:32 +0530

    add(27/10/24)

--- src/components/Webinars.js
+++ src/components/Webinars.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import WebinarTop from './webinar/WebinarTop';
 import Webfeature from './webinar/Webfeature';
+import Webgrow from './webinar/Webgrow';
 
 const Webinars = () => {
   return (
     <div>
       <WebinarTop/>
       <Webfeature/>
+      <Webgrow/>
     </div>
   );
 }
