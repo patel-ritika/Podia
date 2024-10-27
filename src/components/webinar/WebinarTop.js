56d76bdc8f368c4231e8e6529e5150ff4e2b038c
Your Name <you@example.com>
2024-10-27 18:58:42 +0530

    add(27/10/24)

--- src/components/webinar/WebinarTop.js
+++ src/components/webinar/WebinarTop.js
@@ -3,8 +3,11 @@ import "../../asset/styles/webinar/webinartop.css";
 
 const WebinarTop = () => {
   return (
-    <div>
-      hey
+    <div className="webinartop">
+        <h1>Sell webinars, live classes, and workshops</h1>
+        <h4>Go live, get paid. Podia lets you sell access to your webinars on Zoom or<br></br>
+         YouTube Live. And when it’s all over, you can even sell the replays, too.</h4>
+         <button className="webinar">Start selling today</button>
     </div>
   );
 }
